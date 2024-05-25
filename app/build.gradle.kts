069ccb6d8cf4297f74398eb25847b0a86ed16dbf
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-05-24 21:05:24 -0600

    Revert "Feature/anuncio admob"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,5 +43,4 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.github.denzcoskun:ImageSlideShow:0.0.6")
-    implementation("com.google.android.gms:play-services-ads:23.0.0")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        maven("https://maven.google.com")
     }
 }
 
