7274ab3f0138b734630340b8af1cf13bce0682d2
Gene7sis <genesisraneroo@gmail.com>
2024-05-24 23:51:27 -0600

    se creo el carrusel de nuevo y funciona

--- app/src/main/java/com/example/saeta/HomeFragment.kt
+++ app/src/main/java/com/example/saeta/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
+import com.denzcoskun.imageslider.constants.ScaleTypes
 
 class HomeFragment : Fragment() {
 
@@ -13,6 +16,21 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val imageSlider = view.findViewById<ImageSlider>(R.id.slider)
+
+        val slideModels = ArrayList<SlideModel>()
+
+        slideModels.add(SlideModel(R.drawable.foto,ScaleTypes.CENTER_CROP));
+        slideModels.add(SlideModel ("https://fastly.picsum.photos/id/866/500/500.jpg?hmac=FOptChXpmOmfR5SpiL2pp74Yadf1T_bRhBF1wJZa9hg",ScaleTypes.CENTER_CROP));
+        slideModels.add(SlideModel ("https://fastly.picsum.photos/id/270/500/500.jpg?hmac=MK7XNrBrZ73QsthvGaAkiNoTl65ZDlUhEO-6fnd-ZnY",ScaleTypes.CENTER_CROP));
+        slideModels.add(SlideModel ("https://fastly.picsum.photos/id/320/500/500.jpg?hmac=2iE7TIF9kIqQOHrIUPOJx2wP1CJewQIZBeMLIRrm74s",ScaleTypes.CENTER_CROP));
+        slideModels.add(SlideModel ("https://fastly.picsum.photos/id/798/500/500.jpg?hmac=Bmzk6g3m8sUiEVHfJWBscr2DUg8Vd2QhN7igHBXLLfo",ScaleTypes.CENTER_CROP));
+        slideModels.add(SlideModel ("https://fastly.picsum.photos/id/95/500/500.jpg?hmac=0aldBQ7cQN5D_qyamlSP5j51o-Og4gRxSq4AYvnKk2U",ScaleTypes.CENTER_CROP));
+
+        imageSlider.setImageList(slideModels);
+
+        return view
     }
 }
