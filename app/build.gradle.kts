8a73cd51e08e38ed283183a4e6c46aa13b5a78c5
AlexOT03 <alexot855@gmail.com>
2024-05-25 08:23:22 -0600

    add-dependencies-for-api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("com.google.android.gms:play-services-ads:23.0.0")
+    implementation ("com.google.android.gms:play-services-ads:23.1.0")
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
